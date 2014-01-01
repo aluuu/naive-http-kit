f0fb30e860e6d2318ce6554cb5bbd9173425b388
aluuu <zloy.alu@gmail.com>
2014-01-01 18:47:58 +0600

    project metadata fixes

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject naive-http-kit/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Basic http-kit/clojurescript template"
+  :url "https://github.com/aluuu/naive-http-kit"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
